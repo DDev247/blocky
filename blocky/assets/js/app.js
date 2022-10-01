ffd5228083d65270b27ce3e90204812a376cf4f7
DDev247 <dawidgaming.ro@gmail.com>
2022-10-01 10:29:44 +0200

    app canvas init

--- blocky/assets/js/app.js
+++ blocky/assets/js/app.js
@@ -1,7 +1,43 @@
 
+const EDITOR_CANVAS = document.getElementById("editorCanvas");
+const APP_CANVAS = document.getElementById("appCanvas");
+
 function goHome() {
     let link = document.createElement('a');
     link.href = "/blocky/";
     link.click();
     link.remove();
 }
+
+var RunID;
+var FPSID;
+function start() {
+    RunID = setInterval(run, 16);
+    FPSID = setInterval(fps, 1000);
+}
+
+function run() {
+    update();
+    
+    render();
+}
+
+function update() {
+
+}
+
+let frame;
+function render() {
+
+
+    frame++;
+}
+
+let FPS;
+function fps() {
+    FPS = frame;
+    frame = 0;
+    console.log("FPS: " + FPS);
+}
+
+start();
